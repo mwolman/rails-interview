359955d4eea91a91b4c1b40cdbc3044da2dea29e
Micaela Wolman <33032571+mwolman@users.noreply.github.com>
2024-08-21 19:04:09 -0300

    Add factory_bot_rails and faker gems (#9)

--- spec/models/todo_list_spec.rb
+++ spec/models/todo_list_spec.rb
@@ -1,4 +1,11 @@
-RSpec.describe TodoList do
+describe TodoList do
+  describe "factory" do
+    it "has a valid factory" do
+      todo_list = build(:todo_list)
+      expect(todo_list).to be_valid
+    end
+  end
+
   describe "associations" do
     it { is_expected.to have_many(:todo_items).dependent(:destroy) }
   end
